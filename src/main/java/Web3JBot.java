708cbb8c0cb47e6c773ca8ddb5e46a9ae66cfe39
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2024-10-13 07:40:00 +0200

    Update Web3JBot.java

--- src/main/java/Web3JBot.java
+++ src/main/java/Web3JBot.java
@@ -4,14 +4,15 @@ import org.web3j.protocol.core.methods.response.EthBlockNumber;
 
 import java.io.IOException;
 
+// this class implements a hello world using web3j which is currently the most popular and stable Java Ethereum RPC client library
 public class Web3JBot {
 
     public static void main(String[] args) {
-        // Connect to the Ethereum node
-        Web3j web3 = Web3j.build(new HttpService("http://127.0.0.1:8545")); // Use your node URL (e.g., Infura or local Ganache)
+        // Connect to your local Ethereum node
+        Web3j web3 = Web3j.build(new HttpService("http://127.0.0.1:8545")); // uses your node url, geth testnet or anvil (foundry eth suite)
 
         try {
-            // Get the latest block number
+            // Get the latest block number - (blockchain/web3 hello world)
             EthBlockNumber ethBlockNumber = web3.ethBlockNumber().send();
             System.out.println("Latest block number: " + ethBlockNumber.getBlockNumber());
         } catch (IOException e) {
